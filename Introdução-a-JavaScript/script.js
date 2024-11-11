ae062d36e6739342f5343a93cd8f76ccf1adf414
Rauã Hipólito <rauavasconcelos22@gmail.com>
2024-11-11 18:13:28 -0300

    Adicionando variáveis e seus tipos

--- Introdução-a-JavaScript/script.js
+++ Introdução-a-JavaScript/script.js
@@ -1,1 +1,21 @@
-console.log("Agora vaiii");
+// Váriaveis e tipos 
+// Tipo String
+let nome = "Rauã Hipólito";
+// console.log(nome);
+
+// Tipo Number
+let idade = 20;
+// console.log(idade);
+
+// Tipo boolean
+let homem = true;
+let MenorDeIdade = false;
+// console.log(homem);
+
+// Tipo undefined
+let SemValor;
+// console.log(SemValor);
+
+// Tipo Null
+let valorNulo = null;
+// console.log(valorNulo);
