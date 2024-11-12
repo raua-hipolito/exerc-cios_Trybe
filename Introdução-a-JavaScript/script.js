642e42683bfd9288a1251a1c7c3645c78feaee65
Rauã Hipólito <rauavasconcelos22@gmail.com>
2024-11-12 16:48:17 -0300

    ✅ praticando operações aritméticas simples

--- Introdução-a-JavaScript/script.js
+++ Introdução-a-JavaScript/script.js
@@ -19,3 +19,24 @@ let SemValor;
 // Tipo Null
 let valorNulo = null;
 // console.log(valorNulo);
+// --------------------------------------------//
+
+// Operações aritméticas
+// Soma
+console.log(10 + 22); // 32
+
+// Subtração
+console.log(100 - 40); // 60
+
+// Multiplicação
+console.log(4 * 7); // 28
+
+// Divisão
+console.log(40 / 5); // 8
+
+let salary = 6352;
+console.log(salary % 23); //modulo é o tanto que sobrou de uma divisão %
+console.log(salary += 300);
+console.log(salary -= 200);
+
+
